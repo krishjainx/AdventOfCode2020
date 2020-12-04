4ac60c72062bb0e427bfc840b280a8a461b2a8c9
Krish-sysadmin <krishjain02939@gmail.com>
2020-12-04 18:56:09 +0100

    Day 3 done! Paper came in handy

---
+++ Day 3/day3part1.py
@@ -0,0 +1,16 @@
+input = open("Day 3/input.txt").read().splitlines()
+
+xPosition = 0
+encounteredTrees = 0
+
+for i in input:
+    if xPosition >= len(i):
+        xPosition = xPosition - len(i)
+
+    if i[xPosition] == "#":
+        encounteredTrees += 1
+    
+    xPosition += 3
+
+
+print(encounteredTrees)
---
+++ Day 3/day3part2.py
@@ -0,0 +1,16 @@
+input = open("/Users/krishjain/Desktop/AdventOfCode/Day 3/input.txt").read().splitlines()
+
+def day3part2(theInput):
+    def path(xMovement, yMovement):    
+        x = 0
+        total = 0
+        for i in range(len(theInput)):
+            if i % yMovement != 0:         
+                continue
+            if theInput[i][x] == '#':
+                total += 1
+            x = (x + xMovement) % len(theInput[0])
+        return total     
+    return path(1,1) * path(3,1) * path(5,1) * path(7,1) * path(1,2)
+
+print(day3part2(input))
