88a60612c0af560896d7b173382bdcd71b58ed23
Krish-sysadmin <krishjain02939@gmail.com>
2020-12-04 23:03:10 +0100

    Added day4 but escape sequence specific to Linux/mac

---
+++ Day 4/day4part1.py
@@ -0,0 +1,31 @@
+input = open('Day 4/input.txt').read()
+input = input.split("\n\n")
+
+validCounter = 0
+
+for i in input:
+
+    splitBySpaces = i.split()
+
+    detailsNeeded = [
+        'byr',
+        'iyr',
+        'eyr',
+        'hgt',
+        'hcl',
+        'ecl',
+        'pid',
+        ]
+
+    arrayOfTheItems = []
+
+    for j in splitBySpaces:
+        arrayOfTheItems.append(j.split(':')[0])
+
+    doesHaveAllDetails = all(item in arrayOfTheItems for item in
+                             detailsNeeded)
+
+    if doesHaveAllDetails:
+        validCounter = validCounter + 1
+
+print (validCounter)
